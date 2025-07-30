286640a78591202bb5d7b1e88c651906d33a2e93
job ueno <job.ueno.2024@gmail.com>
2025-07-30 23:42:55 +0900

    サンプルで作ってみた Terraform + AWS + ECR + GitHub Actions

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_ecr_repository" "my_repo" {
+  name = "my-repo"
+}
